6ddb56585c89f1e5e851dc3030f3e0dfbb233c6c
Tanish Kumar <119050787+TanishKumar04@users.noreply.github.com>
2023-09-13 19:21:05 +0530

    Product json to js

--- app.js
+++ app.js
@@ -46,3 +46,119 @@ burger.addEventListener("click", function() {
 closeMenu.addEventListener("click", function() {
     menuSidebar.style.transform = "translate(-100%)";
 });
+
+
+class Product{
+  async getProduct(){
+    const response = await fetch("Product.json")
+   const data = await response.json();
+    let Product = data.items;
+    Product = Product.map(items=>{
+        const{title,price} = item.feilds;
+        const{id} = item.sys;
+        const image = item.feilds.image.feilds.file.url;
+        return {title,price,image};
+    })
+    return Product;
+  } 
+
+}
+
+// class UI{
+//     displayProducts(Product){
+//         let result = " ";
+//         Product.forEach(Product=>{
+//             const ProductDiv = document.createElement("div")
+//             ProductDiv.innerHTML ='< div class = "product-cart">
+//                                     <img src ="${Product.image}" alt ="Product"></img>
+//                                     <span class = "add-to-cart" data-id="${Product.id}">
+//                                     <i class = "fa fa-cart--plus fa-1x"
+//                                     style = "margin-right : 0.1em; font-size: 1em;">
+//                                     </i>
+//                                     Add to cart </span>
+//                                     <div class ="Product-name">${Product.title}</div>
+//                                     <div class= "Product-price">$ ${Product.price}</div></div>'
+//             const p = document.querySelector(".product")
+//             p.append(ProductDiv)                        
+
+//         })
+
+//     }
+// }
+
+class UI {
+    displayProducts(products) {
+      let result = " ";
+      products.forEach((product) => {
+        const productDiv = document.createElement("div");
+        productDiv.innerHTML = `
+          <div class="product-cart">
+            <img src="${product.image}" alt="Product">
+            <span class="add-to-cart" data-id="${product.id}">
+              <i class="fa fa-cart-plus fa-1x" style="margin-right: 0.1em; font-size: 1em;"></i>
+              Add to cart
+            </span>
+            <div class="product-name">${product.title}</div>
+            <div class="product-price">$ ${product.price}</div>
+          </div>
+        `;
+        const p = document.querySelector(".product");
+        p.append(productDiv);
+      });
+    } 
+    getButtons(){
+    const btns = document.querySelectorAll(".add-to-cart")
+    buttonDOM = btns;
+    btns.forEach((btn) => {
+        let inCart = cart.find((item) => item.id===id);
+        if(inCart){
+            btn.innerHTML = "In Cart"
+            btn.dissabled = true
+        }
+        btn.addEventListener("click",(e)=>{
+            e.currentTarget.innerHTML = "In Cart"
+            e.currentTarget.style.color = "white"
+            e.currentTarget.style.pointerEvents = "none"
+            let cartItem = {...Storage.getStorageProducts(id),'amount': 1}
+            cart.push(cartItem)
+            Storage.saveCart(cart)
+            this.setCartValues(cart)
+            this.addCartItems(CartItem)
+        })
+    })
+}
+setCartValues(cart){
+    let tempTotal = 0;
+    let itemTotal = 0;
+    Cart.map((item)=>{
+        tempTotal  += (item.price*item.amount);
+        itemTotal += item.amount;
+        parseFloat(tempTotal.toFixed (2))
+    })
+    cartItemsTotal.innerHTML = itemTotal
+    cartItemsTotal.innerHTML = parseFloat(tempTotal.toFixed(2))
+}        
+addCartItem(CartItem){
+    let cartItemUi = document.createElement("div")
+    cartItemUi.innerHTML = `<div class = "cart-product">
+            <div class = "product-image"
+            <img src = "${cartItem.image} alt ="product"
+            </div>
+            <div class ="cart-product-content">
+            <div class ="cart-product-name"><h3>${cartItem.title}
+            </h3></div>
+            <div class = "cart-product-price"><h3>₹${cartItem.price}
+            </h3>
+            </div>
+            <div class ="cart-product-remove data-id="${cartItem.id}"
+            href = "#" style="color:red;">remove</a></div>
+            </div>
+            <div class ="plus-minus">
+            <i class = "fa fa-angle-left add-amount"
+            data-id="${cartItem.id}"></i>
+            `
+}
+
+  }
+  
+ 
